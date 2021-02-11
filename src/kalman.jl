2b48f4fbaf137be25abbd3817b87b8718a83c792
Derek Hansen <dereklh@umich.edu>
2021-02-11 16:41:52 -0500

    got all tests passing again:

--- src/kalman.jl
+++ src/kalman.jl
@@ -132,57 +132,81 @@ end
 
 # Zygote.@nograd kalman_filter_mv
 
-function kalman_smoother_mv(F0, F1, G0, G1, Σ, Tau, μ0, Tau0, Y; mutate=true)
+function kalman_smoother_mv(F0, F1, G0, G1, Σ, Tau, μ0, Tau0, Y)
+    m = KalmanModel(F0=F0, F1=F1, G0=G0, G1=G1, Σ=Σ, Tau=Tau, μ0=μ0, Tau0=Tau0, mutate=true)
+    smooth(m, Y)
+end
+
+function init_smooth_state(::KalmanModel{true}, D, T, ms, Vs)
+    ms_smoothed    = deepcopy(ms)
+    Vs_smoothed    = deepcopy(Vs)
+
+    ms_smoothed[:, 1:(end-1)] .= NaN
+    Vs_smoothed[:, :, 1:(end-1)] .= NaN
+
+    Covs_smoothed = zeros(eltype(ms_smoothed), D, D, T-1)
+    state = @NT(ms_smoothed, Vs_smoothed, Covs_smoothed)
+end
+
+function init_smooth_state(::KalmanModel{false}, D, T, ms, Vs)
+    ms_smoothed   = ms[:, end:end]
+    Vs_smoothed   = Vs[:, :, end:end]
+    Covs_smoothed = Array{eltype(ms_smoothed)}(undef, D, D, 0)
+    state = @NT(ms_smoothed, Vs_smoothed, Covs_smoothed)
+end
+
+function get_smooth_mean_cov(::KalmanModel{true}, state, t)
+    ms_next = view(state.ms_smoothed, :, t+1)
+    Vs_next = view(state.Vs_smoothed, :, :, t+1)
+    return ms_next, Vs_next
+end
+
+function get_smooth_mean_cov(::KalmanModel{false}, state, t)
+    ms_next = view(state.ms_smoothed, :, 1)
+    Vs_next = view(state.Vs_smoothed, :, :, 1)
+    return ms_next, Vs_next
+end
+
+function update_smooth_state!(::KalmanModel{true}, state, t, m_smoothed, V_smoothed, Cov_smoothed)
+    state.ms_smoothed[:, t] = m_smoothed
+    state.Vs_smoothed[:, :, t] = V_smoothed
+    state.Covs_smoothed[:, :, t] = Cov_smoothed
+    return state
+end
+
+function update_smooth_state!(::KalmanModel{false}, state, t, m_smoothed, V_smoothed, Cov_smoothed)
+    ms_smoothed = hcat(m_smoothed, state.ms_smoothed)
+    Vs_smoothed = cat3(V_smoothed, state.Vs_smoothed)
+    Covs_smoothed = cat3(Cov_smoothed, state.Covs_smoothed)
+    state = @NT(ms_smoothed, Vs_smoothed, Covs_smoothed)
+end
+
+function smooth(model::KalmanModel, Y)
+    θ              = model.θ
     T              = size(Y)[1]
 
-    loglik, ms, Vs = kalman_filter_mv(F0, F1, G0, G1, Σ, Tau, μ0, Tau0, Y; mutate=mutate)
+    loglik, filt_state  = model(Y)
+    ms = filt_state.ms
+    Vs = filt_state.Vs
     D              = size(ms, 1)
 
-    if mutate
-        ms_smoothed    = deepcopy(ms)
-        Vs_smoothed    = deepcopy(Vs)
-
-        ms_smoothed[:, 1:(end-1)] .= NaN
-        Vs_smoothed[:, :, 1:(end-1)] .= NaN
-
-        Covs_smoothed = zeros(eltype(ms_smoothed), D, D, T-1)
-    else
-        ms_smoothed   = ms[:, end]
-        Vs_smoothed   = Vs[:, :, end]
-        Covs_smoothed = Array{eltype(ms_smoothed)}(undef, D, D, 0)
-    end
+    state = init_smooth_state(model, D, T, ms, Vs)
 
     if (T > 1)
-        ms_smoothed, Vs_smoothed, Covs_smoothed = let ms_smoothed=ms_smoothed, Vs_smoothed=Vs_smoothed, Covs_smoothed=Covs_smoothed
             for t in (T-1):-1:1
-                if mutate
-                    ms_next = view(ms_smoothed, :, t+1)
-                    Vs_next = view(Vs_smoothed, :, :, t+1)
-                else
-                    ms_next = view(ms_smoothed, :, 1)
-                    Vs_next = view(Vs_smoothed, :, :, 1)
-                end
-                m_smoothed, V_smoothed, Cov_smoothed = backward_smooth_step_mv(G0(t+1), G1(t+1),
-                                                                Tau(t+1),
+                ms_next, Vs_next = get_smooth_mean_cov(model, state, t)
+                m_smoothed, V_smoothed, Cov_smoothed = backward_smooth_step_mv(θ.G0(t+1), θ.G1(t+1),
+                                                                θ.Tau(t+1),
                                                                 view(ms, :, t),
                                                                 view(Vs, :, :, t),
                                                                 ms_next,
                                                                 Vs_next)
-                if mutate
-                    ms_smoothed[:, t] = m_smoothed
-                    Vs_smoothed[:, :, t] = V_smoothed
-                    Covs_smoothed[:, :, t] = Cov_smoothed
-                else
-                    ms_smoothed = hcat(m_smoothed, ms_smoothed)
-                    Vs_smoothed = cat(V_smoothed, Vs_smoothed; dims=3)
-                    Covs_smoothed = cat(Cov_smoothed, Covs_smoothed; dims=3)
-                end
+                state = update_smooth_state!(model, state, t, m_smoothed, V_smoothed, Cov_smoothed)
+
             end
-            ms_smoothed, Vs_smoothed, Covs_smoothed
-        end
     end
 
-    return ms_smoothed, Vs_smoothed, Covs_smoothed
+    return state
 end
 
 # Zygote.@nograd kalman_smoother_mv
