3ad2cae04cdd6ca3461291119c66c0ab59b2274f
Derek Hansen <dereklh@umich.edu>
2021-02-12 11:28:35 -0500

    Added tests for resampler

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,11 @@ using Test
 @testset "kalman" begin
     include("kalman.jl")
 end
+
+@testset "resampler" begin
+    include("resampler.jl")
+end
+
 @testset "smc" begin
     include("smc.jl")
 end
